42597fb161a4f67c449f0ad218b6797b33bce13e
DiChag <chagindmitry1@gmail.com>
2022-08-22 00:24:35 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,12 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const allIngredientsEl = document.querySelector('ul#ingredients');
+
+const ingredient = ingredients.map(element => {
+  const newElement = document.createElement('li');
+  newElement.textContent = element;
+  newElement.classList.add('item');
+  return newElement;
+});
+
+allIngredientsEl.append(...ingredient);
