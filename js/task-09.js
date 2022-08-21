d098cfac724fcdc04acb03e123da0bf986aec7d5
DiChag <chagindmitry1@gmail.com>
2022-08-22 02:41:42 +0300

    add-task9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeBtn = document.querySelector('button.change-color');
+const currentColor = document.querySelector('span.color');
+
+changeBtn.addEventListener('click', function () {
+  const randomColors = getRandomHexColor();
+
+  document.body.style.backgroundColor = randomColors;
+  currentColor.textContent = randomColors;
+
+  console.log(`Current color: ${randomColors}`);
+});
